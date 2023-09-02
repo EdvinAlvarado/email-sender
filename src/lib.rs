94869bd94f5fbd2f0c3095a13d4aff6d72e87640
EdvinAlvarado <ealvarado301@gmail.com>
2023-09-02 15:20:51 -0500

    moved ui_counter to lib

--- src/lib.rs
+++ src/lib.rs
@@ -33,3 +33,16 @@ fn uppercase_first_letter(s: &str) -> String {
 		Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
 	}
 }
+
+fn ui_counter(ui: &mut egui::Ui, counter: &mut i32) {
+	// Put the buttons and label on the same row:
+	ui.horizontal(|ui| {
+		if ui.button("-").clicked() {
+				*counter -= 1;
+		}
+		ui.label(counter.to_string());
+		if ui.button("+").clicked() {
+				*counter += 1;
+		}
+	});
+}
