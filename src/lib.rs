d462b8840fa8fb56466c9d5837d9ca8412981b61
EdvinAlvarado <ealvarado301@gmail.com>
2023-09-02 14:22:53 -0500

    implemented username and fullname

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,35 @@
 
-
-
-pub fn username(email: &str) -> String {
-    todo!()
+pub fn username(email: &str) -> Result<String, Box<dyn Error>> {
+	let mut name_iter = email.split("@").next()?.to_lowercase().split(".");
+	
+	let first_name = name_iter.next()?;
+	let mut last_name: String = name_iter
+		.filter(|s| s.len() > 1)
+		.filter(|s| !s.contains("contractor"))
+		.map(|s| s.replace("-", ""))
+		.map(|s| if s == "ki" {"k"} else {s.as_str()})
+		.collect();
+	
+	Ok(first_name[0]+last_name)
 }
 
 pub fn fullname(email: &str) -> String {
-    todo!()
+	 email
+		.split("@")
+		.next()?
+		.to_lowercase()
+		.replace("contractor", "")
+		.split(".")
+		.map(|s| uppercase_first_letter(s))
+		.collect::<&[&str]>()
+		.join(" ")
+}
+
+// copied
+fn uppercase_first_letter(s: &str) -> String {
+	let mut c = s.chars();
+	match c.next() {
+		None => String::new(),
+		Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+	}
 }
